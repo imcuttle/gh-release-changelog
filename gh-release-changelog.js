828605d3cdba7f14d123bdc3694f497edfb7de97
余聪 <yucong@yuanfudao.com>
2022-01-14 20:33:57 +0800

    chore: wip

---
+++ gh-release-changelog.js
@@ -0,0 +1,198 @@
+const github = require("@actions/github");
+const cp = require("child_process");
+const nps = require("path");
+const fs = require("fs");
+const globby = require("globby");
+const { sync: visitTree } = require("@moyuyc/visit-tree");
+const remark = require("remark");
+const { promisify } = require("util");
+const nodeToString = require("mdast-util-to-string");
+const _readJSON = require("read-json-file");
+const readJSON = promisify(_readJSON);
+
+const execSyncStdout = (cmd) => {
+  try {
+    return cp.execSync(cmd).toString().trim();
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const isVersionText = (heading) => {
+  heading = heading.trim();
+  return /^\d+\.\d+(\.\d+)?/.test(heading) || /^[vV]\d+?/.test(heading);
+};
+
+const isMatchedTag = (heading, tag) => {
+  const normalizedTag = tag.replace(/^v/, "");
+  return (
+    isVersionText(heading) &&
+    (heading.startsWith(normalizedTag) || heading.startsWith(tag))
+  );
+};
+
+const defaultIgnoreTests = [/^<a name=.*><\/a>/];
+
+async function ghReleaseChangelog({
+  changelogFilename,
+  cwd = process.cwd(),
+  tag = execSyncStdout(`git describe --abbrev=0 --tags HEAD`),
+  fromTag,
+  dryRun,
+  githubToken = process.env.GITHUB_TOKEN || process.env.GITHUB_AUTH,
+  repoOwner,
+  repoName,
+  ignoreTests = defaultIgnoreTests,
+  label,
+}) {
+  if (!repoOwner && !repoName) {
+    if (process.env.GITHUB_REPOSITORY) {
+      [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split("/");
+    }
+    if (!repoOwner && !repoName) {
+      const pkgPath = nps.join(cwd, "package.json");
+      if (fs.existsSync(pkgPath) && fs.statSync(pkgPath).isFile()) {
+        const pkg = await readJSON(pkgPath);
+        let repoUrl = pkg.repository;
+        if (repoUrl) {
+          if (typeof repoUrl === "object" && repoUrl.url) {
+            repoUrl = repoUrl.url;
+          }
+
+          const matches = repoUrl.match(
+            /(?:https?|git(?:\+ssh)?)(?::\/\/)(?:www\.)?github\.com\/(.*)/i
+          );
+          if (matches) {
+            [repoOwner, repoName] = matches[1].split("/");
+          } else {
+            [repoOwner, repoName] = repoUrl.split("/");
+          }
+        }
+      }
+    }
+  }
+
+  if (!repoOwner) {
+    throw new Error(`"repoOwner" is required`);
+  }
+  if (!repoName) {
+    throw new Error(`"repoName" is required`);
+  }
+
+  if (!changelogFilename) {
+    const files = await globby(
+      ["CHANGELOG.md", "release.md", "release-note.md", "release-notes.md"],
+      {
+        cwd,
+        onlyFiles: true,
+        caseSensitiveMatch: false,
+        deep: 1,
+        absolute: true,
+      }
+    );
+    changelogFilename = files[0];
+  }
+  if (!changelogFilename) {
+    throw new Error(`"changelogFilename" is required`);
+  }
+
+  changelogFilename = nps.resolve(cwd, changelogFilename)
+  const changelog = await fs.promises.readFile(changelogFilename, "utf-8");
+
+  const nodes = [];
+  let depth;
+  const h = remark().use(() => {
+    return (gnode) => {
+      visitTree(gnode, (node, ctx) => {
+        if (node.type === "heading") {
+          const heading = nodeToString(node).trim();
+          if (isMatchedTag(heading, tag)) {
+            const initialDepth = node.depth;
+            // depth = initialDepth
+            // add next Siblings which is not version heading
+            let index = ctx.index + 1;
+            while (true) {
+              const nextNode = ctx.parent.children[index];
+              index++;
+              if (!nextNode) {
+                break;
+              }
+
+              const text = nodeToString(nextNode);
+              if (
+                nextNode.type !== "heading" ||
+                !isVersionText(text) ||
+                (fromTag && !isMatchedTag(text, fromTag))
+              ) {
+                if (nextNode.type === "heading") {
+                  if (nextNode.depth > 1) {
+                    depth = Math.min(
+                      depth || Number.MAX_VALUE,
+                      nextNode.depth - 1
+                    );
+                  }
+                }
+
+                if (ignoreTests.some((rule) => rule.test(text))) {
+                  continue;
+                }
+                nodes.push(nextNode);
+              } else {
+                break;
+              }
+            }
+
+            depth = depth || initialDepth;
+            ctx.break();
+          }
+        }
+      });
+    };
+  });
+  await h.process(changelog);
+
+  let releaseNote = nodes
+    .map((node) => remark().stringify(node))
+    .join("\n")
+    .trim();
+  if (releaseNote) {
+    let url;
+    if (fromTag) {
+      url = `https://github.com/${repoOwner}/${repoName}/compare/${fromTag}...${tag}`;
+    } else {
+      url = `https://github.com/${repoOwner}/${repoName}/commits/${tag}`;
+    }
+    releaseNote = releaseNote + `\n\nFull Changelog: [${tag}](${url})`;
+
+    if (depth && label) {
+      releaseNote = "#".repeat(depth) + ` ${label}\n\n` + releaseNote;
+    }
+  }
+
+  if (dryRun) {
+    return {
+      releaseNote,
+      changelogFilename,
+      githubToken,
+      repoOwner,
+      repoName,
+      tag,
+      fromTag,
+      label,
+    };
+  } else {
+    if (!releaseNote.trim()) {
+      return;
+    }
+    const octokit = github.getOctokit(githubToken);
+    await octokit.repos.createRelease({
+      owner: repoOwner,
+      repo: repoName,
+      tag_name: tag,
+      body: JSON.parse(releaseNote),
+      draft: true,
+    });
+  }
+}
+
+module.exports = ghReleaseChangelog;
--- index.test.js
+++ index.test.js
@@ -1,24 +1,64 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+const ghReleaseChangelog = require("./gh-release-changelog");
+const process = require("process");
+const cp = require("child_process");
+const path = require("path");
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
+test("ghReleaseChangelog", async () => {
+  const data = await ghReleaseChangelog({
+    cwd: path.resolve("fixture"),
+    tag: "v1.0.0",
+    dryRun: true,
+    githubToken: "noop",
+    label: "@rcp/abc",
+  });
+  expect(data).toMatchInlineSnapshot(`
+    Object {
+      "changelogFilename": "/Users/congyu/github/gh-release-changelog/fixture/CHANGELOG.md",
+      "fromTag": undefined,
+      "githubToken": "noop",
+      "label": "@rcp/abc",
+      "releaseNote": "## @rcp/abc
 
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
+    ### Bug Fixes
+
+    *   should remove container firstly ([4e0d087](https://github.com/imcuttle/rcp/commit/4e0d087))
+    *   should remove container firstly ([8dfcc84](https://github.com/imcuttle/rcp/commit/8dfcc84))
+    *   use extendDictionary ([330edf0](https://github.com/imcuttle/rcp/commit/330edf0))
+    *   use.fetcher with key ([fe45ac1](https://github.com/imcuttle/rcp/commit/fe45ac1))
+    *   use.i18ncontext ([da70207](https://github.com/imcuttle/rcp/commit/da70207))
+
+    ### Features
+
+    *   add c.preventfastop ([44bf84f](https://github.com/imcuttle/rcp/commit/44bf84f))
+    *   add eq option ([631dfe7](https://github.com/imcuttle/rcp/commit/631dfe7))
+    *   add hook ([300eb94](https://github.com/imcuttle/rcp/commit/300eb94))
+    *   add use.i18n ([8f2b256](https://github.com/imcuttle/rcp/commit/8f2b256))
+    *   allow customized eq function ([79e92ce](https://github.com/imcuttle/rcp/commit/79e92ce))
+    *   remove global option ([55f611a](https://github.com/imcuttle/rcp/commit/55f611a))
+    *   use es6 instead of es5 ([3dcc2a8](https://github.com/imcuttle/rcp/commit/3dcc2a8))
+    *   util.createlogger log alias to info ([8757ae8](https://github.com/imcuttle/rcp/commit/8757ae8))
+
+    ### Performance Improvements
+
+    *   set defaultLanguage to defaultProps ([cf01859](https://github.com/imcuttle/rcp/commit/cf01859))
+
+    ### BREAKING CHANGES
+
+    *   use es6 instead of es5
+    *   remove global option
+
+    Full Changelog: [v1.0.0](https://github.com/imcuttle/rcp/commits/v1.0.0)",
+      "repoName": "rcp",
+      "repoOwner": "imcuttle",
+      "tag": "v1.0.0",
+    }
+  `);
 });
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
-  const ip = path.join(__dirname, 'index.js');
-  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+test("test runs", () => {
+  process.env["INPUT_MILLISECONDS"] = 100;
+  const ip = path.join(__dirname, "index.js");
+  const result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
   console.log(result);
-})
+});
